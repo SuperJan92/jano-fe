ef15fd60cab138704f526195784893d65e1c37ac
SuperJan93 <janvanerkel92@gmail.com>
2024-12-29 19:08:47 +0100

    Hamburger icon

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                includePaths: ['./node_modules'],
+                additionalData: `@use "src/styles/main.scss" as *;`,
             },
         },
     },

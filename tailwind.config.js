ccf0791580a663e2eb3924680b1d404a2ca36d54
SuperJan93 <janvanerkel92@gmail.com>
2024-12-29 12:33:46 +0100

    Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,22 @@ export default {
   content: [
     './src/**/*.{astro,html,js}', // Zorg ervoor dat alle bestanden in src goed worden meegenomen
     './src/pages/**/*.{astro,html,js}', // Specifiek voor de pagina's
+    './src/styles/**/*.css', // Zorg dat custom CSS wordt meegenomen
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-orange': '#ff9d20',
+        'custom-gray': '#bdbfc3',
+        'custom-darkgray': '#3a3f47',
+        'custom-lightgray': '#a1a1a1',
+        'custom-bg': '#141a23',
+      },
+      fontFamily: {
+        goldplay: ['Goldplay', 'sans-serif'],
+        opensans: ['OpenSans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};

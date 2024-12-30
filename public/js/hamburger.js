07fcd854c6ac448c404be5dfedc0b1e771d1b12d
SuperJan93 <janvanerkel92@gmail.com>
2024-12-30 14:02:23 +0100

    Hamburger icon menu toggle

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -4,3 +4,12 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const headerContainer = document.querySelector(".header__container");
+    const hamburgerButton = document.querySelector("button.hamburger");
+
+    hamburgerButton.addEventListener("click", () => {
+        headerContainer.classList.toggle("is-open");
+    });
+});
